b71e76261c98cfaac950750098f947db24745a60
allahuphallahupa <its.renatt@gmail.com>
2021-01-10 15:48:04 +0300

    Сделал файл для работы с браузером

--- browser.py
+++ browser.py
@@ -1,15 +1,30 @@
-from selenium import webdriver
+'''Работа с браузером'''
 
-type(webdriver.Safari().get('https://proglib.io/p/web-scraping').find_element(By.CLASS, 'sheet-header__logo flex valign-center'))
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webelement import WebElement 
+
+
+# TODO: шобы многа браузеров могло изспользовать. пока тока сафаре
 class Browser:
 	#Взаимодействие с браузером
 	def __init__(self):
 		#Инициализация веб драйвера
-		pass	
-	
-	def get_page(self):
-		#Получает страничку и добавляет ее в self
-		pass
+		self.driver = webdriver.Safari()
 
-	def get_element(self)->None:
-		pass
+	def click_element(element:WebElement):
+		element.click_element()
+
+	def get_page(page:str, self):
+		#Получает страничку. 
+		self.driver.get(page)
+
+	def get_element(self, xpath:str)->WebElement:
+		#Ждет пока элемент подгрузится и выводит его
+		WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+		element = self.driver.find_element_by_xpath(xpath)
+		return element 
+	def close(self)
+		self.driver.close()
